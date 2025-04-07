2d32aac6c31348c6afcb55db187b1c938f32b63e
Маргарита Рождественская <m.rogdestvenskya@mail.ru>
2025-04-07 20:06:39 +0200

    docs: update readme

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
 import './scss/styles.scss';
+
+export enum PaymentMethod {
+	Online = 'online',
+	Offline = 'offline',
+}
+
+export interface IProduct {
+	id: string;
+	category: string;
+	title: string;
+	image: string;
+	description: string;
+	price: number | string;
+}
+
+export interface IOrder {
+	payment: PaymentMethod;
+	address: string;
+	email: string;
+	phone: string;
+	total: number;
+	items: IProductInBasket[];
+}
+
+export interface IProductsData {
+	products: IProduct[];
+	preview: string | null;
+	getCard(cardId: string): IProduct | undefined;
+}
+
+export interface IOrderData {
+	getOrderInfo(): IBasket;
+	setOrderInfo(orderData: IOrder): void;
+	deleteProduct(basketId: string, payload: Function | null): void;
+	updateOrder(order: IBasket, payload: Function | null): void;
+	checkOrderValidation(
+		data: Record<keyof IPaymentPage, IContactsCustomer>
+	): boolean;
+}
+
+export type IProductItemList = Pick<
+	IProduct,
+	'category' | 'title' | 'image' | 'price'
+>;
+
+export type IProductPopup = Pick<
+	IProduct,
+	'category' | 'title' | 'image' | 'description' | 'price'
+>;
+
+export type IProductInBasket = Pick<IProduct, 'title' | 'price'>;
+
+export type IBasket = Pick<IOrder, 'items' | 'total'>;
+
+export type IPaymentPage = Pick<IOrder, 'payment' | 'address'>;
+
+export type IContactsCustomer = Pick<IOrder, 'email' | 'phone'>;
+
+export type IConfirmationOrder = Pick<IOrder, 'total'>;
